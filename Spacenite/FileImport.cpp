324e9b648f3bfbcf77d0e4e379a87cb1afd705d3
JakeWeston02 <jakeweston02@gmail.com>
2023-01-31 15:53:25 +0000

    added headers + created function

--- Spacenite/FileImport.cpp
+++ Spacenite/FileImport.cpp
@@ -1,4 +1,14 @@
 #include "FileImport.h"
-#include <rapidjson/document.h>
+
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <iostream>
 
 using namespace rapidjson;
+using namespace std;
+
+void FileImport::main()
+{
+	// document.Parse();		json file to be parsed goes as argument
+}
--- Spacenite/FileImport.h
+++ Spacenite/FileImport.h
@@ -4,7 +4,10 @@ class FileImport
 
 public:
 
+	void main();
+
 private:
 
+	Document document;
 };
 
