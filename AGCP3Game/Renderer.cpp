a82155d565fc1ec4ec3c20a7a72c55c34cbee3cc
KieronA <b9027207@my.shu.ac.uk>
2023-02-16 08:54:55 +0000

    Diff renderer class

--- AGCP3Game/Renderer.cpp
+++ AGCP3Game/Renderer.cpp
@@ -1,4 +1,4 @@
-
+#include "stdafx.h"
 #include "Renderer.h"
 
 #include <stdexcept>
@@ -7,72 +7,139 @@
 
 using namespace DirectX;
 
-Renderer::Renderer(HWND hWnd, int width, int height)
-    : m_hWnd(hWnd), m_width(width), m_height(height), m_frameIndex(0)
-{
-    InitD3D(hWnd);
-    LoadAssets();
-}
+#include <DirectXMath.h>
 
-Renderer::~Renderer()
-{
-    WaitForPreviousFrame();
-}
+using namespace Microsoft::WRL;
 
-void Renderer::Update()
-{
+#pragma comment(lib, "d3dcompiler.lib")
 
-}
+//Renderer::Renderer(HWND hWnd, int width, int height)
+//{
+//    // Create the device
+//    CreateDevice();
+//
+//    // Create the swap chain
+//    CreateSwapChain(hWnd, width, height);
+//
+//    // Create the command list
+//    CreateCommandList();
+//
+//    // Create the fence
+//    CreateFence();
+//}
+//
+//Renderer::~Renderer()
+//{
+//    // Wait for the GPU to complete its commands
+//    WaitForPreviousFrame();
+//}
+//
+//void Renderer::Render()
+//{
+//    // Get the current render target
+//    ComPtr<ID3D12Resource> renderTarget = m_renderTargets[m_frameIndex];
+//
+//    // Reset the command allocator and command list
+//    m_commandAllocators[m_frameIndex]->Reset();
+//    m_commandList->Reset(m_commandAllocators[m_frameIndex].Get(), nullptr);
+//
+//    // Transition the render target to the render target state
+//    D3D12_RESOURCE_BARRIER barrierDesc = {};
+//    barrierDesc.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+//    barrierDesc.Transition.pResource = renderTarget.Get();
+//    barrierDesc.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+//    barrierDesc.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
+//    barrierDesc.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+//    m_commandList->ResourceBarrier(1, &barrierDesc);
+//
+//    // Set the viewport and scissor rect
+//    D3D12_VIEWPORT viewport = {};
+//    viewport.TopLeftX = 0.0f;
+//    viewport.TopLeftY = 0.0f;
+//    viewport.Width = static_cast<float>(m_renderTargets[0]->GetDesc().Width);
+//    viewport.Height = static_cast<float>(m_renderTargets[0]->GetDesc().Height);
+//    viewport.MinDepth = 0.0f;
+//    viewport.MaxDepth = 1.0f;
+//    m_commandList->RSSetViewports(1, &viewport);
+//
+//    D3D12_RECT scissorRect = {};
+//    scissorRect.left = 0;
+//    scissorRect.top = 0;
+//    scissorRect.right = static_cast<LONG>(m_renderTargets[0]->GetDesc().Width);
+//    scissorRect.bottom = static_cast<LONG>(m_renderTargets[0]->GetDesc().Height);
+//
+//}
 
-void Renderer::Render()
-{
-    PopulateCommandList();
 
-    ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
-    m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
 
-    WaitForPreviousFrame();
-}
 
-void Renderer::Resize(int width, int height)
-{
-    if (m_swapChain)
-    {
-        WaitForPreviousFrame();
-
-        m_commandList->Reset(m_commandAllocator.Get(), nullptr);
-
-        for (UINT i = 0; i < _countof(m_renderTargets); i++)
-        {
-            m_renderTargets[i].Reset();
-        }
-        m_rtvHeap.Reset();
-
-        m_width = width;
-        m_height = height;
-
-        DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
-        m_swapChain->GetDesc(&swapChainDesc);
-        m_swapChain->ResizeBuffers(swapChainDesc.BufferCount, width, height, swapChainDesc.BufferDesc.Format, swapChainDesc.Flags);
-
-        CreateDescriptorHeaps();
-    }
-}
-
-void Renderer::InitD3D(HWND hWnd)
-{
-    // Create DXGIFactory
-    HRESULT hr = CreateDXGIFactory1(IID_PPV_ARGS(&m_factory));
-    if (FAILED(hr))
-    {
-        throw std::runtime_error("Failed to create DXGI factory");
-    }
-
-    // Create device
-    Microsoft::WRL::ComPtr<IDXGIAdapter1> adapter;
-    D3D_FEATURE_LEVEL featureLevels[] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL_12_0 };
-    for (UINT i
-
+//
+//Renderer::Renderer(HWND hWnd, int width, int height)
+//    : m_hWnd(hWnd), m_width(width), m_height(height), m_frameIndex(0)
+//{
+//    InitD3D(hWnd);
+//    LoadAssets();
+//}
+//
+//Renderer::~Renderer()
+//{
+//    WaitForPreviousFrame();
+//}
+//
+//void Renderer::Update()
+//{
+//
+//}
+//
+//void Renderer::Render()
+//{
+//    PopulateCommandList();
+//
+//    ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
+//    m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+//
+//    WaitForPreviousFrame();
+//}
+//
+//void Renderer::Resize(int width, int height)
+//{
+//    if (m_swapChain)
+//    {
+//        WaitForPreviousFrame();
+//
+//        m_commandList->Reset(m_commandAllocator.Get(), nullptr);
+//
+//        for (UINT i = 0; i < _countof(m_renderTargets); i++)
+//        {
+//            m_renderTargets[i].Reset();
+//        }
+//        m_rtvHeap.Reset();
+//
+//        m_width = width;
+//        m_height = height;
+//
+//        DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+//        m_swapChain->GetDesc(&swapChainDesc);
+//        m_swapChain->ResizeBuffers(swapChainDesc.BufferCount, width, height, swapChainDesc.BufferDesc.Format, swapChainDesc.Flags);
+//
+//        CreateDescriptorHeaps();
+//    }
+//}
+//
+//void Renderer::InitD3D(HWND hWnd)
+//{
+//    // Create DXGIFactory
+//    HRESULT hr = CreateDXGIFactory1(IID_PPV_ARGS(&m_factory));
+//    if (FAILED(hr))
+//    {
+//        throw std::runtime_error("Failed to create DXGI factory");
+//    }
+//
+//    // Create device
+//    Microsoft::WRL::ComPtr<IDXGIAdapter1> adapter;
+//    D3D_FEATURE_LEVEL featureLevels[] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL_12_0 };
+//    for (UINT i
+//
 
 
 
--- AGCP3Game/Renderer.h
+++ AGCP3Game/Renderer.h
@@ -5,40 +5,69 @@
 #include <DirectXMath.h>
 #include <wrl/client.h>
 
-class Renderer
-{
-public:
-    Renderer(HWND hWnd, int width, int height);
-    ~Renderer();
 
-    void Update();
-    void Render();
+//class Renderer
+//{
+//public:
+//    Renderer(HWND hWnd, int width, int height);
+//    ~Renderer();
+//
+//    void Render();
+//
+//private:
+//    void CreateDevice();
+//    void CreateSwapChain(HWND hWnd, int width, int height);
+//    void CreateCommandList();
+//    void CreateFence();
+//    void WaitForPreviousFrame();
+//
+//    Microsoft::WRL::ComPtr<ID3D12Device> m_device;
+//    Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_commandQueue;
+//    Microsoft::WRL::ComPtr<IDXGISwapChain3> m_swapChain;
+//    Microsoft::WRL::ComPtr<ID3D12Resource> m_renderTargets[2];
+//    Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_commandAllocators[2];
+//    Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
+//    Microsoft::WRL::ComPtr<ID3D12Fence> m_fence;
+//    UINT64 m_fenceValue;
+//    HANDLE m_fenceEvent;
+//    UINT m_frameIndex;
+//};
 
-    void Resize(int width, int height);
 
-private:
-    HWND m_hWnd;
-    int m_width;
-    int m_height;
-
-    Microsoft::WRL::ComPtr<IDXGIFactory4> m_factory;
-    Microsoft::WRL::ComPtr<ID3D12Device> m_device;
-    Microsoft::WRL::ComPtr<ID3D12Fence> m_fence;
-    Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_commandQueue;
-    Microsoft::WRL::ComPtr<IDXGISwapChain3> m_swapChain;
-    Microsoft::WRL::ComPtr<ID3D12Resource> m_renderTargets[2];
-    Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_commandAllocator;
-    Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
-    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
-    UINT m_rtvDescriptorSize;
-
-    UINT m_frameIndex;
-
-    void InitD3D(HWND hWnd);
-    void CreateCommandObjects();
-    void CreateSwapChain(HWND hWnd);
-    void CreateDescriptorHeaps();
-    void LoadAssets();
-    void PopulateCommandList();
-    void WaitForPreviousFrame();
-};
+//class Renderer
+//{
+//public:
+//    Renderer(HWND hWnd, int width, int height);
+//    ~Renderer();
+//
+//    void Update();
+//    void Render();
+//
+//    void Resize(int width, int height);
+//
+//private:
+//    HWND m_hWnd;
+//    int m_width;
+//    int m_height;
+//
+//    Microsoft::WRL::ComPtr<IDXGIFactory4> m_factory;
+//    Microsoft::WRL::ComPtr<ID3D12Device> m_device;
+//    Microsoft::WRL::ComPtr<ID3D12Fence> m_fence;
+//    Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_commandQueue;
+//    Microsoft::WRL::ComPtr<IDXGISwapChain3> m_swapChain;
+//    Microsoft::WRL::ComPtr<ID3D12Resource> m_renderTargets[2];
+//    Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_commandAllocator;
+//    Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
+//    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+//    UINT m_rtvDescriptorSize;
+//
+//    UINT m_frameIndex;
+//
+//    void InitD3D(HWND hWnd);
+//    void CreateCommandObjects();
+//    void CreateSwapChain(HWND hWnd);
+//    void CreateDescriptorHeaps();
+//    void LoadAssets();
+//    void PopulateCommandList();
+//    void WaitForPreviousFrame();
+//};
