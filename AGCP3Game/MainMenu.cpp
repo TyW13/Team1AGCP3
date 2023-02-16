c5ae459d9fb82d5d2059a1a700fdf9b0ea8271bd
KieronA <b9027207@my.shu.ac.uk>
2023-02-16 13:48:43 +0000

    test

--- AGCP3Game/MainMenu.cpp
+++ AGCP3Game/MainMenu.cpp
@@ -4,51 +4,51 @@
 //
 //
 //
-#include "SoundCommon.h"
-#include "Audio.h"
-#include "SimpleMath.h"
-#include "WICTextureLoader.h"
-
-using namespace DirectX;
-using namespace DirectX::SimpleMath;
-
-MainMenu::MainMenu(ID3D12Device* device, ID3D12GraphicsCommandList* commandList, float width, float height)
-    : m_device(device), m_commandList(commandList), m_width(width), m_height(height)
-{
-    // Load background image
-    Microsoft::WRL::ComPtr<ID3D12Resource> backgroundResource;
-    HRESULT hr = CreateWICTextureFromFile(device, L"background.png", &backgroundResource, m_background.GetAddressOf());
-    if (FAILED(hr))
-    {
-        throw std::exception("Failed to load background texture.");
-    }
-    m_gameObjects.push_back(m_background);
-
-    // Load title image
-    Microsoft::WRL::ComPtr<ID3D12Resource> titleResource;
-    hr = CreateWICTextureFromFile(device, L"title.png", &titleResource, m_title.GetAddressOf());
-    if (FAILED(hr))
-    {
-        throw std::exception("Failed to load title texture.");
-    }
-    m_title->SetPosition(Vector2(m_width * 0.5f, m_height * 0.25f));
-    m_gameObjects.push_back(m_title);
-
-    // Load start button image
-    Microsoft::WRL::ComPtr<ID3D12Resource> startButtonResource;
-    hr = CreateWICTextureFromFile(device, L"start.png", &startButtonResource, m_startButton.GetAddressOf());
-    if (FAILED(hr))
-    {
-        throw std::exception("Failed to load start button texture.");
-    }
-    m_startButton->SetPosition(Vector2(m_width * 0.5f, m_height * 0.5f));
-    m_gameObjects.push_back(m_startButton);
-
-    // Load background music
-    m_backgroundMusic = std::make_unique<SoundEffect>(m_device, L"background_music.wav");
-    m_backgroundMusicInstance = m_backgroundMusic->CreateInstance(SoundEffectInstance_Use3D);
-    m_backgroundMusicInstance->Play(true);
-}
+//#include "SoundCommon.h"
+//#include "Audio.h"
+//#include "SimpleMath.h"
+//#include "WICTextureLoader.h"
+//
+//using namespace DirectX;
+//using namespace DirectX::SimpleMath;
+//
+//MainMenu::MainMenu(ID3D12Device* device, ID3D12GraphicsCommandList* commandList, float width, float height)
+//    : m_device(device), m_commandList(commandList), m_width(width), m_height(height)
+//{
+//    // Load background image
+//    Microsoft::WRL::ComPtr<ID3D12Resource> backgroundResource;
+//    HRESULT hr = CreateWICTextureFromFile(device, L"background.png", &backgroundResource, m_background.GetAddressOf());
+//    if (FAILED(hr))
+//    {
+//        throw std::exception("Failed to load background texture.");
+//    }
+//    m_gameObjects.push_back(m_background);
+//
+//    // Load title image
+//    Microsoft::WRL::ComPtr<ID3D12Resource> titleResource;
+//    hr = CreateWICTextureFromFile(device, L"title.png", &titleResource, m_title.GetAddressOf());
+//    if (FAILED(hr))
+//    {
+//        throw std::exception("Failed to load title texture.");
+//    }
+//    m_title->SetPosition(Vector2(m_width * 0.5f, m_height * 0.25f));
+//    m_gameObjects.push_back(m_title);
+//
+//    // Load start button image
+//    Microsoft::WRL::ComPtr<ID3D12Resource> startButtonResource;
+//    hr = CreateWICTextureFromFile(device, L"start.png", &startButtonResource, m_startButton.GetAddressOf());
+//    if (FAILED(hr))
+//    {
+//        throw std::exception("Failed to load start button texture.");
+//    }
+//    m_startButton->SetPosition(Vector2(m_width * 0.5f, m_height * 0.5f));
+//    m_gameObjects.push_back(m_startButton);
+//
+//    // Load background music
+//    m_backgroundMusic = std::make_unique<SoundEffect>(m_device, L"background_music.wav");
+//    m_backgroundMusicInstance = m_backgroundMusic->CreateInstance(SoundEffectInstance_Use3D);
+//    m_backgroundMusicInstance->Play(true);
+//}
 //
 //MainMenu::~MainMenu()
 //{
