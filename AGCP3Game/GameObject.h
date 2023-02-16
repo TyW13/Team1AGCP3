fbee51677e64b433ba005ccad2d0573b82d2671b
KieronA <b9027207@my.shu.ac.uk>
2023-02-16 05:18:11 +0000

    Swapped out GameObject for a different sample

--- AGCP3Game/GameObject.h
+++ AGCP3Game/GameObject.h
@@ -1,14 +1,34 @@
 #pragma once
 
-
 #include <DirectXMath.h>
-#include <d3d12.h>
 
 using namespace DirectX;
 
-
 class GameObject
 {
+public:
+    GameObject();
+    GameObject(float x, float y, float width, float height);
+    virtual ~GameObject();
 
+    // Getters and setters
+    float GetX() const { return m_x; }
+    void SetX(float x) { m_x = x; }
+    float GetY() const { return m_y; }
+    void SetY(float y) { m_y = y; }
+    float GetWidth() const { return m_width; }
+    void SetWidth(float width) { m_width = width; }
+    float GetHeight() const { return m_height; }
+    void SetHeight(float height) { m_height = height; }
+
+    // Update and render
+    virtual void Update(float deltaTime);
+    virtual void Render();
+
+    // Check for collision with another game object
+    bool IsColliding(GameObject* other);
+
+protected:
+    float m_x, m_y;
+    float m_width, m_height;
 };
-
