999c960fe3edf367cec703073d3ba5cf75ff7cd1
KieronA <b9027207@my.shu.ac.uk>
2023-02-27 19:41:19 +0000

    Renderer h & cpp complete

--- AGCP3Game/Render.cpp
+++ AGCP3Game/Render.cpp
@@ -1,14 +1,13 @@
 #include "Render.h"
 
+#include "d3dx12.h"
+
 Render::Render(HWND hwnd, int width, int height)
 {
 
 }
 
-void Render::RenderFrame()
-{
 
-}
 
 void Render::CreateDevice()
 {
@@ -50,24 +49,68 @@ void Render::CreateRenderTargetView()
     D3D12_DESCRIPTOR_HEAP_DESC desc = {};
     desc.NumDescriptors = 2;
     desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+
+    m_device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&m_rtvDescriptorHeap));
+
+    m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart());
+
+    for (int i = 0; i < 2; i++)
+    {
+        m_swapChain->GetBuffer(i, IID_PPV_ARGS(&m_renderTargets[i]));
+        m_device->CreateRenderTargetView(m_renderTargets[i], nullptr, rtvHandle);
+        rtvHandle.Offset(1, m_rtvDescriptorSize);
+    }
 }
 
 void Render::CreateCommandAllocator()
 {
-
+    m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator));
 }
 
 void Render::CreateCommandList()
 {
-
+    m_device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator, nullptr, IID_PPV_ARGS(&m_commandList));
+    m_commandList->Close();
 }
 
 void Render::PopulateCommandList()
 {
+    UINT clearColor[] = { 0, 0, 255, 0 };
+    D3D12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex], D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+    m_commandList->ResourceBarrier(1, &barrier);
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart(), m_frameIndex, m_rtvDescriptorSize);
+    m_commandList->OMSetRenderTargets(1, &rtvHandle, FALSE, nullptr);
+    m_commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
+    barrier = CD3DX12_RESOURCE_BARRIER::Transition(m_renderTargets[m_frameIndex], D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+    m_commandList->ResourceBarrier(1, &barrier);
+    m_commandList->Close();
+}
 
+void Render::RenderFrame()
+{
+    PopulateCommandList();
+
+    ID3D12CommandList* ppCommandLists[] = { m_commandList };
+    m_commandQueue->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
+
+    m_swapChain->Present(1, 0);
+
+    WaitForPreviousFrame();
+
+    m_frameIndex = m_swapChain->GetCurrentBackBufferIndex();
 }
 
 void Render::WaitForPreviousFrame()
 {
+    const UINT64 fence = m_fenceValue;
+    m_commandQueue->Signal(m_fence, fence);
+    m_fenceValue++;
 
+    if (m_fence->GetCompletedValue() < fence)
+    {
+        m_fence->SetEventOnCompletion(fence, m_fenceEvent);
+        WaitForSingleObject(m_fenceEvent, INFINITE);
+    }
 }
