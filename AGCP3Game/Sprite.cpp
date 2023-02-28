1ddf3a9a9c87771fb92e5d578b466aecfad70b6e
KieronA <b9027207@my.shu.ac.uk>
2023-02-28 00:22:16 +0000

    Commit before Sprite class change

--- AGCP3Game/Sprite.cpp
+++ AGCP3Game/Sprite.cpp
@@ -2,81 +2,122 @@
 
 #include "Sprite.h"
 #include "DDSTextureLoader.h"
-
+#include "Texture.h"
 #include "ResourceUploadBatch.h"
 
-Sprite::Sprite()
-    : m_texture(nullptr), m_vertexBuffer(nullptr), m_indexBuffer(nullptr), m_size(XMFLOAT2(0.0f, 0.0f))
+void Sprite::LoadFromFile(const std::wstring& filename, Renderer* renderer)
 {
-    XMStoreFloat4x4(&m_transform, XMMatrixIdentity());
+    m_texture = std::make_unique<Texture>(renderer);
+    m_texture->LoadFromFile(filename);
 }
 
-Sprite::~Sprite()
+void Sprite::LoadTexture(ID3D12Device* device, const wchar_t* filename)
 {
-    if (m_texture)
-    {
-        m_texture->Release();
-        m_texture = nullptr;
-    }
 
-    if (m_vertexBuffer)
-    {
-        m_vertexBuffer->Release();
-        m_vertexBuffer = nullptr;
-    }
-
-    if (m_indexBuffer)
-    {
-        m_indexBuffer->Release();
-        m_indexBuffer = nullptr;
-    }
+    m_texture.LoadFromFile(device, filename);
+    
 }
 
-bool Sprite::LoadTextureFromFile(ID3D12Device* device, ID3D12GraphicsCommandList* commandList, const std::wstring& fileName)
+void Sprite::SetTexture(Texture* texture)
 {
-  
-    HRESULT hr = CreateDDSTextureFromFile(device, commandList, fileName.c_str(), &m_texture, &m_textureDescriptorHandle);
-    if (FAILED(hr))
-    {
-        return false;
-    }
-
-    return true;
+    m_texture = std::unique_ptr<Texture>(texture);
 }
 
-void Sprite::Render(ID3D12GraphicsCommandList* commandList, ID3D12DescriptorHeap* heap, UINT descriptorSize, ID3D12RootSignature* rootSignature, ID3D12PipelineState* pipelineState)
+void Sprite::SetTransform(const XMFLOAT2& position, float rotation, const XMFLOAT2& scale)
 {
-    // Set pipeline state and root signature
-    commandList->SetPipelineState(pipelineState);
-    commandList->SetGraphicsRootSignature(rootSignature);
-
-    // Set texture descriptor heap
-    ID3D12DescriptorHeap* descriptorHeaps[] = { heap };
-    commandList->SetDescriptorHeaps(_countof(descriptorHeaps), descriptorHeaps);
-    commandList->SetGraphicsRootDescriptorTable(0, m_textureDescriptorHandle);
-
-    // Set vertex and index buffers
-    commandList->IASetVertexBuffers(0, 1, &m_vertexBufferView);
-    commandList->IASetIndexBuffer(&m_indexBufferView);
-
-    // Set primitive topology
-    commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-
-    // Update constant buffer
-    XMMATRIX world = XMLoadFloat4x4(&m_transform);
-    XMMATRIX view = XMMatrixIdentity;
-
-
+    m_position = position;
+    m_rotation = rotation;
+    m_scale = scale;
 }
 
-void Sprite::SetPosition(float x, float y)
+
+void Sprite::Draw(Renderer& renderer)
 {
+    // Bind the sprite's texture to the pipeline.
+    renderer.SetTexture(m_texture);
+
+    // Set the sprite's transform.
+    XMFLOAT3 position = { 0.0f, 0.0f, 0.0f };
+    XMFLOAT3 scale = { 1.0f, 1.0f, 1.0f };
+    float rotation = 0.0f;
+    renderer.SetTransform(position, scale, rotation);
+
+    // Draw the sprite.
+    renderer.Draw(m_rect);
 }
 
-void Sprite::InitializeVertexBuffer()
-{
-}
-
-void Sprite::InitializeIndexBuffer()
-{
-}
+//Sprite::Sprite()
+//    : m_texture(nullptr), m_vertexBuffer(nullptr), m_indexBuffer(nullptr), m_size(XMFLOAT2(0.0f, 0.0f))
+//{
+//    XMStoreFloat4x4(&m_transform, XMMatrixIdentity());
+//}
+//
+//Sprite::~Sprite()
+//{
+//    if (m_texture)
+//    {
+//        m_texture->Release();
+//        m_texture = nullptr;
+//    }
+//
+//    if (m_vertexBuffer)
+//    {
+//        m_vertexBuffer->Release();
+//        m_vertexBuffer = nullptr;
+//    }
+//
+//    if (m_indexBuffer)
+//    {
+//        m_indexBuffer->Release();
+//        m_indexBuffer = nullptr;
+//    }
+//}
+//
+//bool Sprite::LoadTextureFromFile(ID3D12Device* device, ID3D12GraphicsCommandList* commandList, const std::wstring& fileName)
+//{
+//  
+//    HRESULT hr = CreateDDSTextureFromFile(device, commandList, fileName.c_str(), &m_texture, &m_textureDescriptorHandle);
+//    if (FAILED(hr))
+//    {
+//        return false;
+//    }
+//
+//    return true;
+//}
+//
+//void Sprite::Render(ID3D12GraphicsCommandList* commandList, ID3D12DescriptorHeap* heap, UINT descriptorSize, ID3D12RootSignature* rootSignature, ID3D12PipelineState* pipelineState)
+//{
+//    // Set pipeline state and root signature
+//    commandList->SetPipelineState(pipelineState);
+//    commandList->SetGraphicsRootSignature(rootSignature);
+//
+//    // Set texture descriptor heap
+//    ID3D12DescriptorHeap* descriptorHeaps[] = { heap };
+//    commandList->SetDescriptorHeaps(_countof(descriptorHeaps), descriptorHeaps);
+//    commandList->SetGraphicsRootDescriptorTable(0, m_textureDescriptorHandle);
+//
+//    // Set vertex and index buffers
+//    commandList->IASetVertexBuffers(0, 1, &m_vertexBufferView);
+//    commandList->IASetIndexBuffer(&m_indexBufferView);
+//
+//    // Set primitive topology
+//    commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+//
+//    // Update constant buffer
+//    XMMATRIX world = XMLoadFloat4x4(&m_transform);
+//    XMMATRIX view = XMMatrixIdentity;
+//
+//
+//}
+//
+//void Sprite::SetPosition(float x, float y)
+//{
+//}
+//
+//void Sprite::InitializeVertexBuffer()
+//{
+//}
+//
+//void Sprite::InitializeIndexBuffer()
+//{
+//}
--- AGCP3Game/Sprite.h
+++ AGCP3Game/Sprite.h
@@ -2,46 +2,84 @@
 
 #include <d3d12.h>
 #include <DirectXMath.h>
+#include <memory>
 
 
-
-#include <string>
-
-
-using namespace DirectX;
+class Renderer;
+class Texture;
 
 class Sprite
 {
 public:
-    Sprite();
+    Sprite(Renderer* renderer);
     ~Sprite();
 
-    bool LoadTextureFromFile(ID3D12Device* device, ID3D12GraphicsCommandList* commandList, const std::wstring& fileName);
-    void Render(ID3D12GraphicsCommandList* commandList, ID3D12DescriptorHeap* heap, UINT descriptorSize, ID3D12RootSignature* rootSignature, ID3D12PipelineState* pipelineState);
-    void SetPosition(float x, float y);
+    void LoadFromFile(const std::wstring& filename);
+    void SetTexture(Texture* texture);
+    void SetTransform(const DirectX::XMFLOAT2& position, float rotation, const DirectX::XMFLOAT2& scale);
+
+    void Render();
 
 private:
-    struct Vertex
+    std::unique_ptr<Texture> m_texture;
+    DirectX::XMFLOAT2 m_position;
+    float m_rotation;
+    DirectX::XMFLOAT2 m_scale;
+
+    Renderer* m_renderer;
+
+    struct SpriteVertex
     {
-        XMFLOAT3 position;
-        XMFLOAT2 uv;
+        DirectX::XMFLOAT3 position;
+        DirectX::XMFLOAT2 uv;
     };
 
-    ID3D12Resource* m_texture;
-    D3D12_GPU_DESCRIPTOR_HANDLE m_textureDescriptorHandle;
-
-    ID3D12Resource* m_vertexBuffer;
+    Microsoft::WRL::ComPtr<ID3D12PipelineState> m_pipelineState;
+    Microsoft::WRL::ComPtr<ID3D12RootSignature> m_rootSignature;
+    Microsoft::WRL::ComPtr<ID3D12Resource> m_vertexBuffer;
     D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
-
-    ID3D12Resource* m_indexBuffer;
-    D3D12_INDEX_BUFFER_VIEW m_indexBufferView;
-
-    XMFLOAT2 m_position;
-    XMFLOAT2 m_size;
-    XMFLOAT4X4 m_transform;
-
-    void InitializeVertexBuffer();
-    void InitializeIndexBuffer();
+    
+    void CreateVertexBuffer();
+    void CreatePipelineState();
+    void CreateRootSignature();
 };
+
+
+
+
+
+//class Sprite
+//{
+//public:
+//    Sprite();
+//    ~Sprite();
+//
+//    bool LoadTextureFromFile(ID3D12Device* device, ID3D12GraphicsCommandList* commandList, const std::wstring& fileName);
+//    void Render(ID3D12GraphicsCommandList* commandList, ID3D12DescriptorHeap* heap, UINT descriptorSize, ID3D12RootSignature* rootSignature, ID3D12PipelineState* pipelineState);
+//    void SetPosition(float x, float y);
+//
+//private:
+//    struct Vertex
+//    {
+//        XMFLOAT3 position;
+//        XMFLOAT2 uv;
+//    };
+//
+//    ID3D12Resource* m_texture;
+//    D3D12_GPU_DESCRIPTOR_HANDLE m_textureDescriptorHandle;
+//
+//    ID3D12Resource* m_vertexBuffer;
+//    D3D12_VERTEX_BUFFER_VIEW m_vertexBufferView;
+//
+//    ID3D12Resource* m_indexBuffer;
+//    D3D12_INDEX_BUFFER_VIEW m_indexBufferView;
+//
+//    XMFLOAT2 m_position;
+//    XMFLOAT2 m_size;
+//    XMFLOAT4X4 m_transform;
+//
+//    void InitializeVertexBuffer();
+//    void InitializeIndexBuffer();
+//};
 
 
