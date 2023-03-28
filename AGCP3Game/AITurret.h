7d21290d128356f9b95989b4b10e92fe077fa526
KieronA <b9027207@my.shu.ac.uk>
2023-03-28 10:03:21 +0100

    adding functions to header / private resources/types

--- AGCP3Game/AITurret.h
+++ AGCP3Game/AITurret.h
@@ -9,7 +9,7 @@ class ResourceManager;
 class AITurret : public GameObject
 {
 public:
-	AITurret(DeviceManager* dManager, std::wstring texPath, DirectX::SimpleMath::Vector2 position, DirectX::SimpleMath::Vector2 scale, bool active, DirectX::SimpleMath::Vector2 objSize, std::string objType, bool isCollidable, RECT objRect = {0,0,0,0})
+	AITurret(DeviceManager* dManager, std::wstring texPath, DirectX::SimpleMath::Vector2 position, DirectX::SimpleMath::Vector2 scale, bool active, DirectX::SimpleMath::Vector2 objSize, std::string objType, bool isCollidable, RECT objRect = { 0,0,0,0 })
 		: GameObject(dManager, texPath, position, scale, active, objSize, objType, isCollidable, objRect)
 	{
 		Init(dManager, texPath, position, scale, active, objSize, objType, isCollidable, objRect);
@@ -23,6 +23,32 @@ public:
 	void Render(DeviceManager* dManager) override;
 
 
+	//void UpdateInput
+
+
+	bool GetActive() override { return isActive; }
+	DirectX::SimpleMath::Vector2 GetObjectSize() override { return objSize; }
+	std::string GetObjectType() override { return objType; }
+	bool GetIsCollidable() override { return isCollidable; }
+	RECT GetCollisionBounds() override { return collisionBounds; }
+	virtual DirectX::SimpleMath::Vector2 GetPosition() { return mPos; }
+	virtual DirectX::SimpleMath::Vector2 GetScale() { return mScale; }
+
+
+
+	void SetActive(bool _isActive) override;
+	void SetObjectSize(DirectX::SimpleMath::Vector2 _objSize) override;
+	void SetIsCollidable(bool _isCollidable) override;
+	void SetRect(RECT _objRect) override;
+	void SetPosition(DirectX::SimpleMath::Vector2 _position) override;
+	void SetScale(DirectX::SimpleMath::Vector2 _scale) override;
+
+
+private:
+	Microsoft::WRL::ComPtr<ID3D12Resource> objTex;
+	bool isActive;
+	DirectX::SimpleMath::Vector2 objSize;
+	std::string objType;
 
 
 };
