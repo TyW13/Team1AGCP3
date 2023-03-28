6fa9b3c5c2e1fb84b1e36d493e53af0b5aaefa6e
KieronA <b9027207@my.shu.ac.uk>
2023-03-28 09:59:45 +0100

    New Turret header - based on Player.cpp

--- AGCP3Game/AITurret.h
+++ AGCP3Game/AITurret.h
@@ -3,25 +3,58 @@
 #include "Bullet.h"
 
 
+
+class ResourceManager;
+
 class AITurret : public GameObject
 {
 public:
-	AITurret();
-	virtual ~AITurret();
+	AITurret(DeviceManager* dManager, std::wstring texPath, DirectX::SimpleMath::Vector2 position, DirectX::SimpleMath::Vector2 scale, bool active, DirectX::SimpleMath::Vector2 objSize, std::string objType, bool isCollidable, RECT objRect = {0,0,0,0})
+		: GameObject(dManager, texPath, position, scale, active, objSize, objType, isCollidable, objRect)
+	{
+		Init(dManager, texPath, position, scale, active, objSize, objType, isCollidable, objRect);
+	}
 
 
-	void Update(DeviceManager* dManager, ResourceManager* rManager, float dTime);
-	void Shoot();
 
-private:
 
-	float m_shotTimer;
-	float m_shotDelay;
+	void Init(DeviceManager* dManager, std::wstring texPath, DirectX::SimpleMath::Vector2 position, DirectX::SimpleMath::Vector2 scale, bool active, DirectX::SimpleMath::Vector2 objSize, std::string _objType, bool isCollidable, RECT objRect = { 0, 0, 0 ,0 }) override;
+	void Update(DeviceManager* dManager, ResourceManager* rManager, float dTime) override;
+	void Render(DeviceManager* dManager) override;
 
 
 
 
 };
+
+
+
+
+
+
+
+
+
+
+//class AITurret : public GameObject
+//{
+//public:
+//	AITurret();
+//	virtual ~AITurret();
+//
+//
+//	void Update(DeviceManager* dManager, ResourceManager* rManager, float dTime);
+//	void Shoot();
+//
+//private:
+//
+//	float m_shotTimer;
+//	float m_shotDelay;
+//
+//
+//
+//
+//};
 
 
 
